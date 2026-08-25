9c33ee669fc422c8e9b532341ca27d1e29e5137f
julio659554-cyber <julio.659554@gmail.com>
2026-08-25 10:56:01 -0300

    chore: bump PWA cache for invite flow

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'juhelo-v2';
-const CORE = ['./', './index.html', './styles.css', './styles-v2.css', './app.js', './manifest.webmanifest', './assets/icon.svg'];
+const CACHE = 'juhelo-v3';
+const CORE = ['./', './index.html', './styles.css', './styles-v2.css', './app.js', './couple-widget.js', './manifest.webmanifest', './assets/icon.svg'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE)));
