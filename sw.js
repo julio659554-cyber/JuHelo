546c1bc1d32a44f4158eb6aea1830801b87d2cab
julio659554-cyber <julio.659554@gmail.com>
2026-08-25 23:27:57 -0300

    chore: bump PWA cache to v39

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='juhelo-v38';
-const CORE=['./','./index.html','./app-v31.css?v=37','./modal-ux-v36.css?v=37','./picker-v37.css?v=38','./app-v31.js?v=37','./modal-ux-v36.js?v=37','./picker-v37.js?v=37','./boot-v37.js?v=37','./supabase-proxy-v33.js?v=37','./manifest.webmanifest?v=37','./assets/icon.svg'];
+const CACHE='juhelo-v39';
+const CORE=['./','./index.html','./app-v31.css?v=39','./modal-ux-v36.css?v=39','./picker-v39.css?v=39','./app-v31.js?v=39','./modal-ux-v36.js?v=39','./picker-v39.js?v=39','./boot-v39.js?v=39','./supabase-proxy-v33.js?v=39','./manifest.webmanifest?v=39','./assets/icon.svg'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>Promise.allSettled(CORE.map(url=>cache.add(url)))));
