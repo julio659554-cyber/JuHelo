2fb5f449759e6d4a1b27acbb7a0bff37c5241828
julio659554-cyber <julio.659554@gmail.com>
2026-08-25 21:09:16 -0300

    chore: bump PWA cache to v33

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='juhelo-v32';
-const CORE=['./','./index.html','./app-v31.css?v=32','./app-v31.js?v=32','./boot-v32.js?v=32','./manifest.webmanifest?v=32','./assets/icon.svg'];
+const CACHE='juhelo-v33';
+const CORE=['./','./index.html','./app-v31.css?v=33','./app-v31.js?v=33','./boot-v33.js?v=33','./supabase-proxy-v33.js?v=33','./manifest.webmanifest?v=33','./assets/icon.svg'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>Promise.allSettled(CORE.map(url=>cache.add(url)))));
