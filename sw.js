598ea3107e2bee30f6bab5264b6a22a7c56904d4
julio659554-cyber <julio.659554@gmail.com>
2026-08-26 20:26:24 -0300

    chore: publish final UI UX cache without legacy styles

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='juhelo-uiux-09';
-const CORE=['./','./index.html','./app-v31.css?v=40','./design-system.css?v=uiux9','./app-shell.css?v=uiux9','./app-ui.css?v=uiux9','./forms.css?v=uiux9','./picker.css?v=uiux9','./interactions.css?v=uiux9','./app-v31.js?v=40','./form-ux.js?v=uiux9','./picker.js?v=uiux9','./boot-v40.js?v=40','./supabase-proxy-v33.js?v=40','./manifest.webmanifest?v=uiux9','./assets/icon.svg'];
+const CACHE='juhelo-uiux-10';
+const CORE=['./','./index.html','./design-system.css?v=uiux10','./app-shell.css?v=uiux10','./app-ui.css?v=uiux10','./forms.css?v=uiux10','./picker.css?v=uiux10','./interactions.css?v=uiux10','./app-v31.js?v=uiux10','./form-ux.js?v=uiux10','./picker.js?v=uiux10','./boot.js?v=uiux10','./supabase-proxy-v33.js?v=uiux10','./manifest.webmanifest?v=uiux10','./assets/icon.svg'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>Promise.allSettled(CORE.map(url=>cache.add(url)))));
