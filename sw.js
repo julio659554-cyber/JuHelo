cec1af94a59b0ceb2ad38189ef3be56a60f43720
julio659554-cyber <julio.659554@gmail.com>
2026-08-27 13:46:31 -0300

    chore: refresh PWA cache for uiux12 interactions

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='juhelo-uiux-11-auth1';
-const CORE=['./','./index.html','./design-system.css?v=uiux11','./app-shell.css?v=uiux11','./app-ui.css?v=uiux11','./forms.css?v=uiux11','./picker.css?v=uiux11','./interactions.css?v=uiux11','./app-v31.js?v=uiux11','./form-ux.js?v=uiux11','./picker.js?v=uiux11','./boot.js?v=uiux11','./supabase-proxy-v33.js?v=uiux11-auth1','./manifest.webmanifest?v=uiux11','./assets/icon.svg'];
+const CACHE='juhelo-uiux-12';
+const CORE=['./','./index.html','./design-system.css?v=uiux12','./app-shell.css?v=uiux12','./app-ui.css?v=uiux12','./forms.css?v=uiux12','./picker.css?v=uiux12','./interactions.css?v=uiux12','./app-v31.js?v=uiux12','./form-ux.js?v=uiux12','./picker.js?v=uiux12','./boot.js?v=uiux12','./supabase-proxy-v33.js?v=uiux12-auth1','./manifest.webmanifest?v=uiux12','./assets/icon.svg'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>Promise.allSettled(CORE.map(url=>cache.add(url)))));
