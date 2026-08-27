66528d590e185e34db3af0d237a777c3407f379a
julio659554-cyber <julio.659554@gmail.com>
2026-08-27 00:11:58 -0300

    build: bump PWA cache to uiux11

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='juhelo-uiux-10';
-const CORE=['./','./index.html','./design-system.css?v=uiux10','./app-shell.css?v=uiux10','./app-ui.css?v=uiux10','./forms.css?v=uiux10','./picker.css?v=uiux10','./interactions.css?v=uiux10','./app-v31.js?v=uiux10','./form-ux.js?v=uiux10','./picker.js?v=uiux10','./boot.js?v=uiux10','./supabase-proxy-v33.js?v=uiux10','./manifest.webmanifest?v=uiux10','./assets/icon.svg'];
+const CACHE='juhelo-uiux-11';
+const CORE=['./','./index.html','./design-system.css?v=uiux11','./app-shell.css?v=uiux11','./app-ui.css?v=uiux11','./forms.css?v=uiux11','./picker.css?v=uiux11','./interactions.css?v=uiux11','./app-v31.js?v=uiux11','./form-ux.js?v=uiux11','./picker.js?v=uiux11','./boot.js?v=uiux11','./supabase-proxy-v33.js?v=uiux11','./manifest.webmanifest?v=uiux11','./assets/icon.svg'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>Promise.allSettled(CORE.map(url=>cache.add(url)))));
